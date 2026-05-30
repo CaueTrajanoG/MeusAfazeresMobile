d57977fe9f3e7a54ed927dc8f1a08c0c39bcd1ff
Douglas Carneiro <douglas.ul@gmail.com>
2026-05-30 16:43:08 -0300

    Style: Darkened background color to Violet 100 for better component contrast

--- app/src/main/java/com/example/meusafazeres/ui/theme/Color.kt
+++ app/src/main/java/com/example/meusafazeres/ui/theme/Color.kt
@@ -7,7 +7,7 @@ val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val BrandViolet = Color(0xFF5B21B6) // Even Darker Violet (Tailwind violet-800)
-val BrandLightPurple = Color(0xFFF5F3FF) // Even lighter Background (Tailwind violet-50)
+val BrandLightPurple = Color(0xFFEDE9FE) // Slightly darker background (Tailwind violet-100)
 val BrandWhite = Color(0xFFFFFFFF)
 val BrandDarkViolet = Color(0xFF4C1D95) // Deepest Violet (Tailwind violet-900)
 
